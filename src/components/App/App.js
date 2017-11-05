6a770336fdc7b748fee6216c1c019def785f726a
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-11-05 15:05:14 -0200

    add css font-size tricks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Pomodoro</h1>
+        <h1 className='App__title'>Pomodoro</h1>
       </div>
     )
   }
