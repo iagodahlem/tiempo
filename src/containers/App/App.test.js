833ede59d6c5adef3af2eb5d9b25e84eb9c10a04
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-11-12 18:12:47 -0200

    update App test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -4,7 +4,15 @@ import App from './App'
 
 describe('<App />', () => {
   it('renders without crashing', () => {
+    const props = {
+      setTimer: () => {},
+      startTimer: () => {},
+      stopTimer: () => {},
+      lapse: 0,
+      running: false,
+    }
+
     const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
+    ReactDOM.render(<App {...props} />, div)
   })
 })
