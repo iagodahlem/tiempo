3cb2d60caa1d62d7a76a3b50cfe6cf8a1b851c1f
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-11-12 13:08:14 -0200

    move App to containers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker'
 import configureStore from './store/configureStore'
-import { App } from './components'
+import { App } from './containers'
 import './index.css'
 
 const store = configureStore()
