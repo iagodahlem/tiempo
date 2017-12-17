95b57dad4c72becae5482f50c4569396a2208fb0
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-12-17 18:22:29 -0200

    adds normalizeService

---
+++ src/services/normalizeService.js
@@ -0,0 +1,13 @@
+export const allIds = (arr) => arr
+  .map(item => item.id)
+
+export const byId = (arr) => arr
+  .reduce((obj, item) => ({
+    ...obj,
+    [item.id]: item,
+  }), {})
+
+export default {
+  allIds,
+  byId,
+}
