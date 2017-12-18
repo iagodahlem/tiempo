52e05ae7d0477e4f4e7f43ed7358ee990aaae1b8
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-12-18 10:43:17 -0200

    adds typesReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import timer from './timerReducer'
+import types from './typesReducer'
 
 const rootReducer = combineReducers({
   timer,
+  types,
 })
 
 export default rootReducer
