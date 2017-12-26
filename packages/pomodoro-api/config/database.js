86f2710f3a4462f9d5805e6df2a9de61ce209360
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-12-26 11:06:00 -0200

    adds setup for db with sequelize

---
+++ packages/pomodoro-api/config/database.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: 'pomodoro',
+    password: null,
+    database: 'pomodoro-dev',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'pomodoro',
+    password: null,
+    database: 'pomodoro-test',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+}
