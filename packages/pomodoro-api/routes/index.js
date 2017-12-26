3cf131732e997c0b13e86b0a9dc64d1bf7b77e5b
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-12-26 11:48:37 -0200

    updates base route to return a string

--- packages/pomodoro-api/routes/index.js
+++ packages/pomodoro-api/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (_, res) => {
-  res.json({ data: 'Server is up.' })
+  res.json('Server is up.')
 })
 
 export default router
