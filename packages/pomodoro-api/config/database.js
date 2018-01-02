c5a9abf3e7f56ca7f2b4dd34ba8a875400b6839b
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2018-01-02 10:54:12 -0200

    adjusting database config

--- packages/pomodoro-api/config/database.js
+++ packages/pomodoro-api/config/database.js
@@ -1,34 +1,32 @@
-const config = {
-  logging: false,
-  operatorsAliases: false,
-}
-
 module.exports = {
   development: {
-    ...config,
     username: 'pomodoro',
     password: null,
     database: 'pomodoro-dev',
     host: '127.0.0.1',
     port: 5432,
     dialect: 'postgres',
+    operatorsAliases: false,
+    logging: false,
   },
   test: {
-    ...config,
     username: 'pomodoro',
     password: null,
     database: 'pomodoro-test',
     host: '127.0.0.1',
     port: 5432,
     dialect: 'postgres',
+    operatorsAliases: false,
+    logging: false,
   },
   production: {
-    ...config,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
     port: process.env.DB_PORT,
     dialect: 'postgres',
+    operatorsAliases: false,
+    logging: false,
   },
 }
