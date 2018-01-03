d43f65456a41e2a860f22a661ace757a8bed2166
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2018-01-03 10:49:08 -0200

    adjust database config for production

--- packages/pomodoro-api/config/database.js
+++ packages/pomodoro-api/config/database.js
@@ -20,11 +20,7 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
+    env: 'DATABASE_URL',
     dialect: 'postgres',
     operatorsAliases: false,
     logging: false,
