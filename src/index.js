76e21e01603e72db532f6a7071ef71bef4946775
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2019-02-08 13:30:15 -0200

    Enable service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { combineReducers } from 'redux'
+import { sessionReducer, timerReducer } from 'timer/store'
 import createStore from './createStore'
 import { configureContainer } from './container'
-import { sessionReducer, timerReducer } from 'timer/store'
 import Root from './Root'
 import * as serviceWorker from './serviceWorker'
 import './index.css'
@@ -30,4 +30,4 @@ ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.register()
